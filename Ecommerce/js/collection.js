4a9877998f675b94c08a751aa0e8c1329fdc2fe5
Rowan Haver <haverrowan@gmail.com>
2022-02-13 17:26:51 +0000

    Added a js folder and collection.js file where i put in a function which dynamically changes if number is 2, this is just temporary as it will be used to change the href of the user button when a user is logged in

---
+++ Ecommerce/js/collection.js
@@ -0,0 +1,13 @@
+/*
+aim is to check login and change href link, (replace number with check login using session id) 
+if user is logged in change the href link to users page where they can edit and check there basket.
+*/
+number = 2;
+let signinCheck = () => {
+    if(number == 2){
+        let a = document.getElementById("signin");
+        a.href = "Sign-in.html";
+    }
+    //add an else if client is not logged in it will direct them to the the regular signin page
+
+}
